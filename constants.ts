b070b75bcba30858f64850d82bbbbd57197e9ed1
meit-shah123 <meitshah@gofynd.com>
2025-07-30 17:13:08 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,48 +1,53 @@
 
-export const COMPONENT_LIST = `
-Avatar: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43197-4112
-Accordion: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43215-76908
-Badge: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43215-99021
-Bottom Sheet: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43266-55270
-Breadcrumbs: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43215-100568
-Button: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43210-25074
-Card: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-324400
-Checkbox: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43550-88502
-Counter Badge: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-333345
-Datepicker: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-289911
-Divider: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-336832
-Drag and Drop: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43282-94242
-Fab Button: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=29566-658
-Feedback: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=21050-11127
-Filter Pill: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=36106-98169
-Form Navigation: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18036-546788
-Form Stepper: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18994-292639
-Icon: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=7080-83221
-IconButton: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18009-61487
-Image: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=62584-159309
-Input Field: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=518-154894
-Link: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=17638-593271
-Menu: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=9884-149235
-Modal: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=9352-121702
-Notification Banner: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=17856-77143
-Notification Toast: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?m=auto&node-id=39052-50230&t=PTsHsp3dOZ8uNdrc-1
-Pagination: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=629-158025
-Popover: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=28608-50092
-Progress Bar: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18221-13552
-Prompt Bar-Regular: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=48054-914
-Radio: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=62696-158640
-Scrollbar: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=45346-294363
-Search: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=9932-334396
-Multi Select: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=49301-29028
-Selector Dropdown: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=19798-280464
-Single Select: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=49264-32760&t=TzgLZMMC9sjXn9RR-11
-Multi Select: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=49301-29028&t=TzgLZMMC9sjXn9RR-11
-Slider: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=39325-8581
-Spinner: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=5274-70519
-Swipe Button: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=21055-4349
-Table Header: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=36033-74902
-Tabs: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=28812-38305
-Tag: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=19913-660170
-Time Picker: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=46023-34086
-Toggle: https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=62215-177038
-`;
+export interface ComponentInfo {
+  name: string;
+  url: string;
+  previewUrl: string | null;
+}
+
+export const COMPONENT_DATA: ComponentInfo[] = [
+  { name: 'Avatar', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43197-4112', previewUrl: null },
+  { name: 'Accordion', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43215-76908', previewUrl: null },
+  { name: 'Badge', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43215-99021', previewUrl: null },
+  { name: 'Bottom Sheet', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43266-55270', previewUrl: null },
+  { name: 'Breadcrumbs', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43215-100568', previewUrl: null },
+  { name: 'Button', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43210-25074', previewUrl: 'https://res.cloudinary.com/dld2c5dex/image/upload/fl_preserve_transparency/v1753856627/Screenshot_2025-07-30_at_11.31.59_AM_ckjk9e.jpg?_s=public-apps' },
+  { name: 'Card', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-324400', previewUrl: null },
+  { name: 'Checkbox', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43550-88502', previewUrl: null },
+  { name: 'Counter Badge', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-333345', previewUrl: null },
+  { name: 'Datepicker', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-289911', previewUrl: null },
+  { name: 'Divider', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43269-336832', previewUrl: null },
+  { name: 'Drag and Drop', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=43282-94242', previewUrl: null },
+  { name: 'Fab Button', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=29566-658', previewUrl: null },
+  { name: 'Feedback', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=21050-11127', previewUrl: null },
+  { name: 'Filter Pill', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=36106-98169', previewUrl: null },
+  { name: 'Form Navigation', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18036-546788', previewUrl: null },
+  { name: 'Form Stepper', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18994-292639', previewUrl: null },
+  { name: 'Icon', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=7080-83221', previewUrl: null },
+  { name: 'IconButton', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18009-61487', previewUrl: null },
+  { name: 'Image', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=62584-159309', previewUrl: null },
+  { name: 'Input Field', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=518-154894', previewUrl: null },
+  { name: 'Link', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=17638-593271', previewUrl: null },
+  { name: 'Menu', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=9884-149235', previewUrl: null },
+  { name: 'Modal', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=9352-121702', previewUrl: null },
+  { name: 'Notification Banner', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=17856-77143', previewUrl: null },
+  { name: 'Pagination', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=629-158025', previewUrl: null },
+  { name: 'Popover', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=28608-50092', previewUrl: null },
+  { name: 'Progress Bar', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=18221-13552', previewUrl: null },
+  { name: 'Prompt Bar-Regular', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=48054-914', previewUrl: null },
+  { name: 'Radio', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=62696-158640', previewUrl: null },
+  { name: 'Scrollbar', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=45346-294363', previewUrl: null },
+  { name: 'Search', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=9932-334396', previewUrl: null },
+  { name: 'Multi Select', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=49301-29028', previewUrl: null },
+  { name: 'Selector Dropdown', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=19798-280464', previewUrl: null },
+  { name: 'Slider', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=39325-8581', previewUrl: null },
+  { name: 'Spinner', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=5274-70519', previewUrl: null },
+  { name: 'Swipe Button', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=21055-4349', previewUrl: null },
+  { name: 'Table Header', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=36033-74902', previewUrl: null },
+  { name: 'Tabs', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=28812-38305', previewUrl: null },
+  { name: 'Tag', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=19913-660170', previewUrl: null },
+  { name: 'Time Picker', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=46023-34086', previewUrl: null },
+  { name: 'Toggle', url: 'https://www.figma.com/design/cjM14Nqi5ewHwIvbssheQK/Novus---Components-1?node-id=62215-177038', previewUrl: null },
+];
+
+export const COMPONENT_LIST = COMPONENT_DATA.map(c => `${c.name}: ${c.url}`).join('\n');
